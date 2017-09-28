fba6a4008520f0f48b4b2368563783d26300edcd
yuhaihui8913 <125227112@qq.com>
2017-09-28 17:35:20 +0800

    编写知识库页面

---
+++ src/components/knowledgeBase/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <x-header :left-options="{showBack: false}"   >知识库</x-header>
+    <flexbox >
+      <flexbox-item><div ><group><selector title="属种大类" v-model="defaultVal" :options="szdlList"></selector></group></div></flexbox-item>
+      <flexbox-item><div ><group><selector title="属种" v-model="defaultVal" :options="szList"></selector></group></div></flexbox-item>
+
+
+    </flexbox>
+
+  </div>
+
+
+</template>
+<script>
+  import {XHeader,TransferDom,Flexbox,FlexboxItem,Selector,Group} from 'vux'
+  export default {
+    directives: {
+      TransferDom
+    },
+    components: {
+      XHeader,
+      Selector,
+      Flexbox,
+      FlexboxItem,
+      Group
+    },
+    data(){
+      return {
+        defaultVal: '',
+        szdlList: [{key: '1',value: 'AAA'},{key: '2',value: 'bbb'}],
+        szList: []
+      }
+    }
+  }
+</script>
+<style lang="less">
+  @import '~vux/src/styles/1px.less';
+  .flex-demo {
+    text-align: center;
+    color: #fff;
+    background-color: #20b907;
+    border-radius: 4px;
+    background-clip: padding-box;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,20 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+//import VueRouter from 'vue-router'
 import App from './App'
-import Home from './components/HelloFromVux'
+//import Home from './components/HelloFromVux'
+import router from './router/index'
 
-Vue.use(VueRouter)
+//Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  component: Home
-}]
-
-const router = new VueRouter({
-  routes
-})
+// const routes = [{
+//   path: '/',
+//   component: Home
+// }]
+//
+// const router = new VueRouter({
+//   routes
+// })
 
 FastClick.attach(document.body)
 
