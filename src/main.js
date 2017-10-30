d0befcdecd127a4c79d01d9126fd36a34a76e447
yuhaihui <125227112@qq.com>
2017-10-30 22:07:09 +0800

    ss

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-//import VueRouter from 'vue-router'
 import App from './App'
-//import Home from './components/HelloFromVux'
 import router from './router/index'
 import { AlertPlugin, ToastPlugin,ConfigPlugin,WechatPlugin } from 'vux'
 
